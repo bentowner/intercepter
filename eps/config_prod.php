40c7428204960abe355bcdc736dfd9cca3b3dc78
Ben Towner <ben.towner@equinix.com>
2016-09-07 16:28:20 -0400

    local config

---
+++ eps/config_prod.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://184.73.15.112/intercept/eps/');
+define('HTTP_CATALOG', 'http://184.73.15.112/intercept/');
+define('HTTP_HELP', 'http://184.73.15.112/intercept/eps/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://184.73.15.112/intercept/eps/');
+define('HTTPS_CATALOG', 'http://184.73.15.112/intercept/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/intercepter/eps/');
+define('DIR_SYSTEM', '/var/www/html/intercepter/system/');
+define('DIR_DATABASE', '/var/www/html/intercepter/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/intercepter/eps/language/');
+define('DIR_TEMPLATE', '/var/www/html/intercepter/eps/view/template/');
+define('DIR_CONFIG', '/var/www/html/intercepter/system/config/');
+define('DIR_IMAGE', '/var/www/html/intercepter/image/');
+define('DIR_CACHE', '/var/www/html/intercepter/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/intercepter/download/');
+define('DIR_LOGS', '/var/www/html/intercepter/system/logs/');
+define('DIR_CATALOG', '/var/www/html/intercepter/catalog/');
+define('DIR_UPLOAD', '/var/www/html/intercepter/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/intercepter/system/modification/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'eps');
+define('DB_PASSWORD', 'eps2!');
+define('DB_DATABASE', 'eps');
+define('DB_PREFIX', 'eps_');
+define('DB_PREFIX_APP', 'epspe_');
